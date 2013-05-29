52b72dd9c0ba1feb7374289dc30b30a008de2371
Lars Schulz <lars.schulz@systemic.de>
2013-05-29 18:52:11 +0200

    Initial

---
+++ assets/js/_scripts.js
@@ -0,0 +1,12 @@
+// Swiper
+
+$(function(){
+	var mySwiper = $('.swiper-container').swiper({
+		//Your options here:
+		mode:'horizontal',
+		loop: true
+		//etc..
+	});
+	
+})
+
---
+++ assets/js/rapid.js
@@ -0,0 +1,15 @@
+/* jQuery Plugin */
+/* @include _jquery-1.9.0.min */
+
+/* CSS3 Media Queries */
+/* @include _respond.min */
+
+/* retina.js (provide image alternative | my_image@2x.png) */
+/* @include _retina */
+
+/* @include _flat-ui */
+
+/* @include swiper */
+
+/* @include _scripts */
+
