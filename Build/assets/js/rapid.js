3b56b1829d143b7f804b9da66a9bb7d24d2c9bfe
Lars Schulz <lars.schulz@systemic.de>
2013-05-31 12:15:06 +0200

    Modal WIP

--- Build/assets/js/rapid.js
+++ Build/assets/js/rapid.js
@@ -1490,11 +1490,13 @@ $(function(){
 	
 	var swiperN11 = $('.swiper-n11').swiper({
 	        pagination: '.pagination-n11',
+	        createPagination: false,
 	        loop: true,
 	        slidesPerSlide: 1
 	    });
 	var swiperN22 = $('.swiper-n22').swiper({
 	        pagination: '.pagination-n22',
+	        createPagination: false,
 	        slidesPerSlide: 1,
 	        mode: 'vertical'
 	    });
